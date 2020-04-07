27f2263319c7faf4f1614616e32d7bd8c436660c
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 08:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  ranks.parseInt;
   
   for (let i = 0; i < ranks.length; i++){
     ranks[i].innerHTML = ranks[i].parseInt + n
