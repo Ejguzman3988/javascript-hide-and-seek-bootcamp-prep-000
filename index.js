2f8ce23d5bd8e94dbd3312fd02fb308b43e4ba41
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 07:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function increaseRankBy(n){
   const ranks = document.getElemebtById('ranked-list').querySelectorAll('li')
   
   for (let i = 0; i < ranks.length; i++){
-    ranks[i] = rank[i].parseInt + num
+    ranks[i].innerHTML = rank[i].parseInt + num
   }
+  
+}
+
+function deepestChild(){
   
 }
