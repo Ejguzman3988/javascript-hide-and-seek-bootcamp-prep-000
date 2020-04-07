2671ec398b121f868f981417e77856b06cc87ff1
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 08:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function deepestChild(){
     
     if (Array.isArray(current)){
       for (let i = 0; i < current.length; i++){
-        next.push()
+        next.push(current[i])
       }
     }
   }
