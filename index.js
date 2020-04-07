abececca9e1dab6611dd83de15ebc1a96bec2fad
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 07:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').querySelector('Div Div Div')
+  return document.getElementById('nested').querySelectorAll('Div')
 }
 
 function increaseRankBy(n){
